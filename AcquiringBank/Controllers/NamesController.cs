f60c25fc5e951b475f68024e84184a9bd186b29c
Paul <paul@pallen.me.uk>
2022-06-21 20:26:33 +0100

    Add project files.

---
+++ AcquiringBank/Controllers/NamesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AcquiringBank.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NamesController : ControllerBase
+    {
+        private readonly string[] _names = {"Bert", "Ethel"};
+
+        [HttpGet("{index}")]
+        public string Get(int index)
+        {
+            return _names[index];
+        }
+    }
+}
---
+++ PaymentGateway/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PaymentGateway.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<string> TestGet()
+        {
+            using var client = new HttpClient();
+            var request = new HttpRequestMessage();
+
+            request.RequestUri = new Uri("http://acquiringbank/api/Names/1");
+            var response = await client.SendAsync(request);
+            var name = await response.Content.ReadAsStringAsync();
+            return name;
+        }
+    }
+}
