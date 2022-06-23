1b03802b6690360787db8348356551ab642503ed
Paul <paul@pallen.me.uk>
2022-06-23 19:48:02 +0100

    Add a Blazor website driver

---
+++ Website/Data/PaymentGatewayService.cs
@@ -0,0 +1,17 @@
+﻿namespace Website.Data
+{
+    public class PaymentGatewayService
+    {
+        public async Task<string> TestGet(int index)
+        {
+            using var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                RequestUri = new Uri($"http://paymentgateway/api/Test/{index}")
+            };
+            var response = await client.SendAsync(request);
+            var name = await response.Content.ReadAsStringAsync();
+            return name;
+        }
+    }
+}
